d42c05b76d6207d74c4a587a69565374a7ed6705
Boyi Qu <boyiqu9596@gmail.com>
2022-09-26 17:36:02 -0700

    Completed retrieving all todos, working on updating a todo

--- datastore/index.js
+++ datastore/index.js
@@ -28,19 +28,69 @@ exports.create = (text, callback) => {
 };
 
 exports.readAll = (callback) => {
-  var data = _.map(items, (text, id) => {
-    return { id, text };
+  let source = exports.dataDir;
+  //read directory
+  //get todo files
+  //
+  fs.readdir(source, (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    //console.log('data ---->', data);
+    let responseData = data.map((item) => {
+      //get rid of extension in each id// get the file name without the extensions
+      let id = path.parse(item).name;
+      let text = id;
+      console.log(id, text);
+      return {id, text};
+    });
+    callback(null, responseData);
   });
-  callback(null, data);
+
+  // var data = _.map(items, (text, id) => {
+  //   return { id, text };
+  // });
+
 };
 
 exports.readOne = (id, callback) => {
-  var text = items[id];
-  if (!text) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback(null, { id, text });
-  }
+  //find dir by id
+  //get message from file
+  //send message/ content of the file to server
+  let source = exports.dataDir;
+  fs.readdir(source, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('data-------->', data);
+      let idExist = false;
+      for (let i = 0; i < data.length; i++) {
+        if (path.parse(data[i]).name === id) {
+          console.log('id is, ', id, 'the found file is', data[i]);
+          idExist = true;
+          fs.readFile(path.join(exports.dataDir, data[i]), (err, fileData)=> {
+            if (err) {
+              console.log(err);
+            } else {
+              callback(null, fileData);
+
+            }
+          });
+          return;
+        }
+        if (!idExist) {
+          callback(err, id);
+        }
+      }
+    }
+  });
+
+  // var text = items[id];
+  // if (!text) {
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback(null, { id, text });
+  // }
 };
 
 exports.update = (id, text, callback) => {
